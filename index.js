f1378b49a211c4f89069f2965fdc8a001a0d09d0
Jacob Rathbun <jrathofbun@gmail.com>
2023-03-26 18:07:26 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+    let newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    let newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    let newCats = cats.slice(0, -1);
+    return newCats;
+}
+
+function removeFirstCat() {
+    let newCats = cats.slice(1);
+    return newCats;
+}
